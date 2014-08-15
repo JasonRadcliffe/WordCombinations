8ee0796399ec3f89097c873d40d92385c44b0f03
JasonRadcliffe <jasonjradcliffe@gmail.com>
2014-08-15 11:05:09 -0400

    Basic file reading

--- keywords/src/keywords/Keywords.java
+++ keywords/src/keywords/Keywords.java
@@ -13,8 +13,29 @@ public class Keywords {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String inFileName = "words.txt";
+        String[] keyword = new String[8];
+        int lineCount = 0;
+        
+        try{
+            Scanner inputStream = new Scanner(new File(inFileName));
+            
+            while(inputStream.hasNextLine()){                
+                keyword[lineCount] = inputStream.nextLine();
+                System.out.println(keyword[lineCount]);
+                
+                lineCount++;
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("The file was not found...");
+        }
         
         
+        
+        System.out.println("OK:\nHere is the 5th line:");
+        System.out.println(keyword[4]);
     }
     
 }
